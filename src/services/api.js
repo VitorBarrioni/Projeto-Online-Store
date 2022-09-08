f43b25e415ee824fe821222ae6e3354703f61250
Camila Mandai <cayumandai@gmail.com>
2022-09-08 15:54:26 -0300

    implementa requisito 1

--- src/services/api.js
+++ src/services/api.js
@@ -1,9 +1,15 @@
 export async function getCategories() {
-  // Implemente aqui
+  const ENDPOINT = 'https://api.mercadolibre.com/sites/MLB/categories';
+  const request = await fetch(ENDPOINT);
+  const result = await request.json();
+  return result;
 }
 
-export async function getProductsFromCategoryAndQuery(/* categoryId, query */) {
-  // Implemente aqui! Quando o fizer, descomente os parâmetros que essa função recebe
+export async function getProductsFromCategoryAndQuery(categoryId, query) {
+  const ENDPOINT = `https://api.mercadolibre.com/sites/MLB/search?category=${categoryId}_ID&q=${query}`;
+  const request = await fetch(ENDPOINT);
+  const result = await request.json();
+  return result;
 }
 
 export async function getProductById() {
