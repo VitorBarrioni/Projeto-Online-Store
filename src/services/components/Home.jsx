d4a8c74bf0d98bc36d302621e1688bfb2795cc12
Sérgio Lima de Souza Filho <sfilhofut@gmail.com>
2022-09-08 16:39:59 -0300

    Requisito 03

--- src/services/components/Home.jsx
+++ src/services/components/Home.jsx
@@ -1,12 +1,13 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 
 export default class Home extends Component {
   render() {
     return (
       <div>
         <input type="text" />
+        <Link data-testid="shopping-cart-button" to="/cart">Carrinho</Link>
         <p data-testid="home-initial-message">
-          {' '}
           Digite algum termo de pesquisa ou escolha uma categoria.
         </p>
       </div>
